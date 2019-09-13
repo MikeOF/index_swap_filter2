1a5ba932d3b9d8bf26becaf71016ed2d04c1ba98
MichaelFinlaysonCUMC <mf3189@cumc.columbia.edu>
2019-09-13 14:57:12 -0400

    testing files.cpp

--- files.cpp
+++ files.cpp
@@ -1,5 +1,7 @@
 #include "files.h"
 
+#define LENGTH 0x1000
+
 int main(int argc, char ** argv) {
 
 	// parse and validate arguments
@@ -17,7 +19,43 @@ int main(int argc, char ** argv) {
 	std::cout << argv[1] << std::endl ;
 
 	if (S_ISDIR(mode)) { std::cout << "it is a dir" << std::endl ; }
-	if (S_ISREG(mode)) { std::cout << "it is a regular file" << std::endl ; }
+	if (S_ISREG(mode)) { 
+		std::cout << "it is a regular file" << std::endl ;
+
+		std::string fpath = std::string(argv[1]) ;
+
+		if (fpath.find(".gz") != std::string::npos) {
+
+		    gzFile file ;
+		    file = gzopen (fpath.c_str(), "r") ;
+
+			while (1) {
+
+				int err;                    
+				int bytes_read ;
+				unsigned char buffer[LENGTH] ;
+				bytes_read = gzread (file, buffer, LENGTH - 1) ;
+				buffer[bytes_read] = '\0' ;
+				printf ("%s", buffer) ;
+
+				if (bytes_read < LENGTH - 1) {
+					if (gzeof (file)) {
+						break;
+					} else {
+						const char * error_string;
+						error_string = gzerror (file, & err);
+						if (err) {
+							fprintf (stderr, "Error: %s.\n", error_string);
+							exit (EXIT_FAILURE);
+						}
+					}
+			}
+		}
+
+		gzclose (file) ;
+
+		}
+	}
 	if (S_ISLNK(mode)) { std::cout << "it is a link" << std::endl ; }
 
 	return 0;
--- files.h
+++ files.h
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zlib.h>
+#include <errno.h>
 
 bool is_file(std::string) ;
 
