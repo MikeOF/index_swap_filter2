0312a7d63f590ce98883591b65367b25b434739b
MichaelFinlaysonCUMC <mf3189@cumc.columbia.edu>
2019-10-11 16:09:21 -0400

    fix bug

--- label_suspects.cpp
+++ label_suspects.cpp
@@ -86,6 +86,16 @@ string align_suspect_reads_task_func(Task<string, Align_suspect_reads_args> task
 	return sam_path.to_string() ;
 }
 
+string get_cug_key(string& line) {
+	int first_tab_pos = line.find('\t') ;
+	int second_tab_pos = line.find('\t', first_tab_pos + 1) ;
+	stringstream ss ;
+	ss << line.substr(0, first_tab_pos) ;
+	ss << "_" ;
+	ss <<  line.substr(first_tab_pos + 1, second_tab_pos - first_tab_pos + 1) ;
+	return ss.str() ;
+}
+
 int parse_sorted_cug_labels_task_func(Task<int, Parse_sorted_cug_labels_args> task) {
 
 	// Parse arguments
@@ -141,12 +151,40 @@ int parse_sorted_cug_labels_task_func(Task<int, Parse_sorted_cug_labels_args> ta
 
 		while(getline(sam_file, line)) {
 
-			cout << line << endl ;
+			// get read id
+			int read_id_stop = line.find(SUSPECT_FASTQ_READ_ID_DELIM) ;
+			string& read_id = line.substr(0, read_id_stop) ;
 
+			// get sample key
+			int sample_key_start = read_id_stop + 1 ;
+			int sample_key_stop = line.find(SUSPECT_FASTQ_READ_ID_DELIM, sample_key_start) ;
+			string& sample_key = line.substr(sample_key, sample_key_stop - sample_key_start) ;
+
+			// get barcode
+			int barcode_start = sample_key_stop + 1 ;
+			int barcode_stop = line.find('\t', barcode_start) ;
+			string& barcode = line.substr(barcode_start, barcode_stop - barcode_start) ;
+
+			// get transcript id & gene id
+			int transcript_id_start = line('\t', barcode_stop + 1) + 1 ;
+			int transcript_id_stop = line('\t', transcript_id_start) ;
+			string& transcript_id = line.substr(transcript_id_start, transcript_id_stop - transcript_id_stop) ;
+			string& gene_id = gene_id_by_transcript_id.at(transcript_id) ;
+
+			// write cug line
+			stringstream ss_key ;
+			ss_key << barcode << "_" << gene_id ; 
+			stringstream ss_line ;
+			ss_line << barcode << '\t' << gene_id << '\t' << sample_key << '\t' << read_id ;
+
+			gz_csw_out.write_line(ss_key.str(), ss_line.str()) ;
 		}
 		sam_file.close() ;
+		gz_csw_out.flush_close() ;
 
 	} else { throw runtime_error("unable to open sam file: " + sam_path) ; }
+
+	collect_sorted_chunks<string>(cug_label_path, gz_csw_out.get_files(), get_cug_key) ;
 
 	return 0 ;
 }
